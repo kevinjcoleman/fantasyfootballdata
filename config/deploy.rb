793253ff48af10b8bfc8ae12a4778b9dfd630da3
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-07-23 16:59:21 -0700

    Change production settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :deploy_to, '/home/deploy/fantasyfootballdata'
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/application.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '54.183.148.144', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
