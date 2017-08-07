718033adeebca005b0e9dcdc06dbaf7c2684bb71
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-08-06 17:11:23 -0700

    Up capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.8.2"
+lock "3.9.0"
 
 set :application, 'fantasyfootballdata'
 set :repo_url, 'git@github.com:kevinjcoleman/fantasyfootballdata.git'
