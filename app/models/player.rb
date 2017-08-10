7a93ece31aed3140e2d4368213445815e3d2ee0a
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-08-10 16:28:41 -0700

    Validate espn key if espn key exists.

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,8 +3,8 @@ class Player < ApplicationRecord
   has_many :player_seasons
 
   validates :yahoo_key, uniqueness: :true
-  validates :espn_id, uniqueness: :true
-  
+  validates :espn_id, uniqueness: :true, unless: :espn_id_blank?
+
   def self.add_from_website(hsh, team)
     where(espn_id: hsh[:espn_id]).first_or_create.tap do |plyr|
       RosterParser::ROW_HEADERS.values.push(:espn_slug).each do |attr|
@@ -37,6 +37,10 @@ class Player < ApplicationRecord
 
   def columns_to_symbols(array)
     array.map {|v| v.to_sym }
+  end
+
+  def espn_id_blank?
+    espn_id
   end
 
   def stats_url
