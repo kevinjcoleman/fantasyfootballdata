033ad5904e5803b43e0e1a02ba28219f20a8e525
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-08-14 11:56:36 -0700

    Got sentry working in dev, testing production now.

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Raven.configure do |config|
-  config.dsn = 'https://36e71880da914ef0929c2380563f806c:0200e54ce2ee41a395760e79697be129@sentry.io/203494'
-end
-
 module Fantasyfootballdata
   class Application < Rails::Application
     config.assets.precompile << 'delayed/web/application.css'
