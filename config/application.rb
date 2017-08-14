f2ce843951f16a278642b0e063efc23a9e9b0528
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-08-14 11:20:36 -0700

    Move sentry block

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Raven.configure do |config|
+  config.dsn = 'https://36e71880da914ef0929c2380563f806c:0200e54ce2ee41a395760e79697be129@sentry.io/203494'
+end
+
 module Fantasyfootballdata
   class Application < Rails::Application
     config.assets.precompile << 'delayed/web/application.css'
@@ -15,8 +19,5 @@ module Fantasyfootballdata
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    Raven.configure do |config|
-      config.dsn = 'https://36e71880da914ef0929c2380563f806c:0200e54ce2ee41a395760e79697be129@sentry.io/203494'
-    end
   end
 end
